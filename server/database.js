237e33e3fbbf6f966029436118ebda764139562f
Anton <golubin1990@gmail.com>
2021-08-19 16:43:03 -0400

    refactored getAllReservations  to use psql

--- server/database.js
+++ server/database.js
@@ -51,11 +51,7 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  // const userId = Object.keys(users).length + 1;
-  // user.id = userId;
-  // users[userId] = user;
-  // return Promise.resolve(user);
-  
+
   const select = 'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *';
   const values = [user.name, user.email, user.password]
   return pool
@@ -73,7 +69,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+  const select = `SELECT properties.*, reservations.*, avg(rating) as average_rating
+                  FROM reservations
+                  JOIN properties ON reservations.property_id = properties.id
+                  JOIN property_reviews ON properties.id = property_reviews.property_id
+                  WHERE reservations.guest_id = $1
+                  AND reservations.end_date < now()::date
+                  GROUP BY properties.id, reservations.id
+                  ORDER BY reservations.start_date
+                  LIMIT $2;
+                  `
+  return pool.
+  query(select, [guest_id, limit])
+  .then(result => result.rows)
+  .catch(error => console.error(error.message))
+
 }
 exports.getAllReservations = getAllReservations;
 
@@ -86,12 +97,10 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
-
   return pool
       .query(`SELECT * FROM properties LIMIT $1`, [limit])
-      .then((result) => result.rows)
+      .then((result) => result.rows[0])
       .catch((error) => console.error(error.message));
-
 };
 
 exports.getAllProperties = getAllProperties;
